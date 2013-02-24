b33b7b73394b55cee6f3773d4ff8a7eb9827ee1f
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-02-24 11:43:20 +0100

    Added manifest for Lync installation

---
+++ manifests/init.pp
@@ -0,0 +1,11 @@
+# Public: Install Microsoft Lync to /Applications.
+#
+# Examples
+#
+#   include lync
+class lync {
+  package { 'Microsoft Lync':
+    provider => 'appdmg',
+    source   => 'https://yum.cegeka.be/iso/Lync_Mac_2011_MAC_MultiLang.dmg',
+  }
+}
