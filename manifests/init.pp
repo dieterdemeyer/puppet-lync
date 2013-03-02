7c67cca5fb91c1dd7390b320346cf85484645955
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-03-02 18:19:04 +0100

    Modified source url

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class lync {
   package { 'Lync_Mac_2011_MAC_MultiLang.pkg.dmg':
     provider => 'pkgdmg',
-    source   => 'https://yum.cegeka.be/iso/Lync_Mac_2011_MAC_MultiLang.pkg.dmg',
+    source   => 'https://yum.cegeka.be/osx_packages/Lync_Mac_2011_MAC_MultiLang.pkg.dmg',
   }
 }
