63dc080d4002ae26a77d44c0f4b38cf75daf5da7
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-09-29 18:58:46 +0200

    Change download location

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,10 @@
 #
 #   include lync
 class lync {
+
   package { 'Lync_Mac_2011_MAC_MultiLang.pkg.dmg':
     provider => 'pkgdmg',
-    source   => 'https://yum.cegeka.be/osx_packages/Lync_Mac_2011_MAC_MultiLang.pkg.dmg',
+    source   => 'https://dl.dropboxusercontent.com/s/tv00er3gqle2xzw/Lync_Mac_2011_MAC_MultiLang.pkg.dmg',
   }
+
 }
